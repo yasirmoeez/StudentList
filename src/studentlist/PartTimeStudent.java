aa638671aefa7f2d02d3f4bab016641a721c833f
yasirmoeez <yasirm@sheridancollege.ca>
2021-01-26 00:21:56 -0500

    Added PT student

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,25 @@
+
+package studentlist;
+
+/**
+ *
+ * @author moeez
+ */
+public class PartTimeStudent extends Student{
+    private int numCourses;
+
+    public PartTimeStudent(int numCourses, String name) {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+    
+}
